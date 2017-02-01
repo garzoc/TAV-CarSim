84d6d0d7c57224a87baddfe5f8f9987a745408b4
IArason <gusarais@student.gu.se>
2017-02-01 10:51:08 +0100

    Update interface method description

--- ActionsInterface.java
+++ ActionsInterface.java
@@ -13,36 +13,33 @@ interface ActionsInterface {
      * Description:
      * This method is used to make the car move forward
      * Pre-Condition:
-     * 1. Car is on the road and
-     * 2. Car is not parked
-     * 3. Car isn't at the end of the street
+     * 1. Car is not parked
+     * 2. Car isn't at the end of the street
      * Post-Condition:
      * 1. Car is moved forward by 1 meters
-     * 2. Method returns position of the car and If the space on the right side is free
+     * 2. Method returns position of the car
      * Test-Cases:
      * 1. @Test moveForward()
-     * 2.
      */
 
 
 
     //---------------------
 
-    void isEmpty();
+    boolean isEmpty();
 
     /**
      * Description:
      * This method Checks is there is an empty parking space.
      * Pre-Condition:
-     * 1. Car is on the road and
-     * 2. Car is not parked
-     * 3. Car is moving forward
+     * 1. Car is not parked
+     * 2. Car is moving forward
      * Post-Condition:
-     * 1. The empty space added to a list
+     * 1. Returns true if adjacent space is empty
      * Test-Cases:
      * 1. @Test isEmpty()
      * 2. Test sensor outputs
-     * 3. Test the noise filtering
+     * 3. Test noise filtering
      */
 
     //---------------------
@@ -53,9 +50,8 @@ interface ActionsInterface {
      * Description:
      * This method is used for moving the car backwards
      * Pre-Condition:
-     * 1. Car is on the road and
-     * 2. Car is not parked
-     * 3. Car is not at the beginning of the street
+     * 1. Car is not parked
+     * 2. Car is not at the beginning of the street
      * Post-Condition:
      * 1. The car gets moved 1 meter backwards
      * Test-Cases:
@@ -71,10 +67,8 @@ interface ActionsInterface {
      * Description:
      * This method is used for the car to be able to park in a free parking space
      * Pre-Condition:
-     * 1. Car is on the road and
-     * 2. Car is not parked
-     * 3. An empty space is found
-     * 4. The car is positioned on the road at the beginning of the free parking space
+     * 1. Car is not parked
+     * 2. The car is positioned on the road at the beginning of a free parking space
      * Post-Condition:
      * 1. The car gets parked in a parking space
      * Test-Cases:
@@ -100,19 +94,17 @@ interface ActionsInterface {
 
     //---------------------
 
-    void WhereIs();
+    int WhereIs();
 
     /**
      * Description:
      * This method is used for keeping track of the cars position on the street as well as if the car is parked or not
      * Post-Condition:
-     * 1.Method shoiuld return the position of the car and whether or not it's parked
+     * 1.Method returns the position of the car
      * Test-Cases:
      * 1. @Test Whereis()
      * 2.
      */
 
     //---------------------
-
-
 }
