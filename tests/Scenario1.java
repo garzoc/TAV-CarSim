0e6b107a3ac5cc5bb86c3f412b9988272e86471e
garzoc <ost@airmail.cc>
2017-02-17 19:19:47 +0100

    added comments to scenarios

--- tests/Scenario1.java
+++ tests/Scenario1.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyInt;
 
 import java.io.BufferedReader;
@@ -19,7 +20,9 @@ import org.junit.Before;
 import org.junit.Test;
 public class Scenario1 {
 	
-	
+	/*
+    reading the external file as our map
+     */
 	public double[]read(String filename){
 		try {
 			FileReader fileReader=new FileReader(filename);
@@ -30,11 +33,9 @@ public class Scenario1 {
 			int i=0;
 			while((line=bufferReader.readLine())!=null&&i<500){
 				lines[i]=(int)Double.parseDouble(line);
-				//lines.add(Double.parseDouble(line));
 				i++;
 			}
 			bufferReader.close();
-			//return lines.toArray(new Double[lines.size()]);
 			return lines;
 			
 		} catch (FileNotFoundException e) {
@@ -48,70 +49,137 @@ public class Scenario1 {
 			
 	}
 	
+	
+	
     /*
-     * Test to see if the car can move forward in a normal situation
+     * This scenario is take from the TAV Assignment Phase 2
+     * 1. Starts at the beginning of the street,  
+     * 2. Moves along the street to the end of the road and scan the available parking places, 
+     * 3. Moves backwards until the most efficient parking place (the smallest available parking where it can still park safely), 
+     * 4. Parks the car, 
+     * 5. Unparks the car and drive to the end of the street. 
+
      * */
 	Sensor sensors[];
 	Actuator actuator;
 	Car pa;
 	@Before
 	public void create(){
+		/*
+		 * Mock to sensors from the sensor interface
+		 * */
 		Sensor sensors[]=new Sensor[2];
 		sensors[0]=mock(Sensor.class);
 		sensors[1]=mock(Sensor.class);
+		/*
+		 * Mock the actuator interface
+		 * */
 		actuator=mock(Actuator.class);
+		/*
+		 * Initiate car to position zero and isParked to false
+		 * */
 		pa=new Car(0,false,sensors,actuator);
+		
 		this.sensors=sensors;
+		/*
+		 * Generate a random map this will be not be used since the sensors are stubbed
+		 * */
 		pa.generateMap(null);
+		/*
+		 * Read VALUES from MAP.txt and to an array of doubles
+		 * */
 		double[] map=read("map/MAP.txt");
-		
+		/*
+		 * Stub the moveF method so that it return 1 for all integers
+		 * */
 		when(actuator.moveF(new State(anyInt(),false))).thenReturn(1);
-		//when(actuator.moveF(new State(499,false))).thenReturn(0);
 		
+		/*
+		 * Stub the moveF method so that it return 1 for whenever the state of the car is passed in
+		 * */
 		when(actuator.reverse(pa.WhereIs())).thenReturn(-1);
+		/*
+		 * Stub the moveF method so that it return 1 for whenever the car is positioned at zero
+		 * */
 		when(actuator.reverse(new State(0,false))).thenReturn(0);
 		
-		//System.out.println(actuator.moveF(new State(499,false)));
+		/*
+		 * Stub the read method from sensors so that it return the value from the map that was read from the "MAP" text file
+		 * */
 		for(int i=0;i<sensors.length;i++)
 			for(int n=0;n<500;n++)
 				when(sensors[i].read(pa.spaces, n)).thenReturn(map[n]);
 		
-		
 	
-//		for(int i=250;i<pa.spaces.length;i++)
-//			when(sensors[0].read(pa.spaces,i)).thenReturn(0.0);
-//		
 	}
 	
 	@Test
 	public void test(){
-		
+		 boolean test=true;
 		//pa.generateMap(null);
-		
+		/*
+		 * Move the car to the end of he street and at the same time scan for available parking spaces
+		 * */
 		for(int i=0;i<499;i++){
 			pa.MoveForward();
 		}
-		
+		/*
+		 * verify position
+		 * */
+		test=test && pa.WhereIs().position==499;
+		/*
+		 * find the most efficient parking space
+		 * (the smallest possible  space where the car can park)
+		 * */
 		FreeSpace optimal=pa.freeSpaces.getFirst();
+		/*
+		 * for every space in freeSpaces (a list of spaces where the car can park ) check if 
+		 * the size of the optimal space is greater the the size of the space in the list then
+		 * set the optimal space to the space in the list
+		 * */
 		for(int i=1;i<pa.freeSpaces.size();i++){
 			if(pa.freeSpaces.get(i).size<optimal.size){
 				optimal=pa.freeSpaces.get(i);
 			}
 		}
 		
+		
+		
+		/*
+		 * Move the car back until the beginning of the most efficient space is reached
+		 * */
 		for(int i=0;i<499-optimal.position;i++){
 			pa.MoveBackward();
 		}
 		
-		
+		/*
+		 * ones reached park the car
+		 * */
 		pa.Park();
-		//System.out.println("pos "+pa.WhereIs().position);
-		//System.out.println(pa.WhereIs().position);
+		
+		/*
+		 * test if car parked successfully
+		 * */
+		test=test && pa.WhereIs().isParked && pa.WhereIs().position==optimal.position+5;
+		/*
+		 * then unpark the car
+		 * */
 		pa.UnPark();
 		
+		/*
+		 * then move to the end of the road.
+		 * */
 		for(int i=pa.WhereIs().position;i<499;i++){
 			pa.MoveForward();
 		}
-		assert(true);
+		
+		/*
+		 * test if car moved to the end of the road
+		 * */
+		test=test && pa.WhereIs().position==499;
+		/*
+		 * verify test
+		 * */
+		assertTrue(test);
 	}
 }
--- tests/Scenario2.java
+++ tests/Scenario2.java
@@ -58,66 +58,105 @@ public class Scenario2 { //rename to Scenario1, mock classes are actually test i
      * It might not find an empty space, at which case it returns to the start
      * When it moves 20 steps, one sensor breaks.
      * */
+    
+    
     Sensor sensors[];
     Actuator actuator;
     Car pa;
     @Before
     public void create(){
+    	/*
+         * Test to see if the car can move forward in a normal situation
+         * */
         Sensor sensors[]=new Sensor[2];
         sensors[0]=mock(Sensor.class);
         sensors[1]=mock(Sensor.class);
         this.sensors=sensors;
-       
+        /*
+		 * Mock to sensors from the sensor interface
+		 * */
         actuator=mock(Actuator.class);
+        /*
+         * generate a random position for the car to start on
+         * */
         double rand=Math.ceil(Math.random()*470);
+        
         int randInt=(int)rand;
+        /*
+		 * Initiate car to a random position and isParked to true
+		 * */
         pa=new Car(randInt,true,sensors,actuator);
-        
+        /*
+		 * Generate a random map this will be not be used since the sensors are stubbed
+		 * */
         pa.generateMap(null);
+        /*
+		 * Read VALUES from MAP.txt and to an array of doubles
+		 * */
         double[] map=read("map/MAP.txt");
-        
+        /*
+		 * Stub the moveF method so that it return 1 for all integers
+		 * */
         when(actuator.moveF(new State(anyInt(),false))).thenReturn(1);
+        /*
+		 * Stub the moveF method so that it return 1 for whenever the state of the car is passed in
+		 * */
         when(actuator.reverse(pa.WhereIs())).thenReturn(-1);
-        
+        /*
+		 * Stub the read method from sensors so that it return the value from the map that was read from the "MAP" text file
+		 * */
         for(int i=0;i<sensors.length;i++)
             for(int n=0;n<500;n++)
                 when(sensors[i].read(pa.spaces, n)).thenReturn(map[n]);
 
 
-
-        for(int i=pa.WhereIs().position+5;i<pa.WhereIs().position+25;i++)
+        
+        /*
+         * stub one of the sensors so that if fails one the car has moved forward 20 time by providing out of bound
+         * values.
+         * After the car has move forward an additional 5 times the sensor will be reactivated
+         * */
+        for(int i=pa.WhereIs().position+20;i<pa.WhereIs().position+25;i++)
             when(sensors[0].read(pa.spaces,i)).thenReturn(0.0);
         
       
-      
-        //double p=sensors[0].read(pa.spaces,249);
-       // System.out.println("test "+p);
+    
 
     }
 
     @Test
     public void test(){
     	
-    	//System.out.println("initail pos "+pa.WhereIs().position);
-        
+    	/*
+    	 * since the car is parked we unpark it
+    	 * */
         pa.UnPark();
+        /*
+         * we then move the car forward ones
+         * */
         pa.MoveForward();
+        /*
+         * continue to move the car forward until it reaches the end of the current parking space
+         * */
         while(pa.WhereIs().streak!=0)
         	pa.MoveForward();
         
-        
+        /*
+         * then park the car in the next available space
+         * */
        pa.Park();
+       /*
+        * the car should end up in one of three possible position 499, 251 ,465 
+        * if car is at position 499 then the car is not parked
+        * if it ended up in any of the other two position the the car should be parked
+        * */
        boolean state=(pa.WhereIs().position==499 && !pa.WhereIs().isParked)||( (pa.WhereIs().position==251 || pa.WhereIs().position==465) && pa.WhereIs().isParked);
-       //System.out.println(pa.WhereIs().position+" "+pa.WhereIs().isParked);
+       /*
+        * assert the test
+        * */
        assertTrue(state);
 
 
 
-        //System.out.println("pos is "+pa.WhereIs().position);
-
-
-        //assertSame(10,obj.get());
-
-        //verify(sensors[1],times(5)).read(pa.spaces,1);
     }
 }
